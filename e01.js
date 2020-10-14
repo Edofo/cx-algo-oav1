fd2d4041cabc97527c331dcf5213a33e70f32920
Nolan <Edofo@users.noreply.github.com>
2020-10-14 09:33:50 +0200

    Update folder

---
+++ e01.js
@@ -0,0 +1,14 @@
+function CamelCase(str) {
+
+    let test = 1;
+
+    for (let i = 0; i < selectObject.options.length; i++) {
+
+        if (selectObject.options[i].selected) {
+            test++;
+        }
+
+      }
+
+    return test;
+}
---
+++ main.js
@@ -0,0 +1,5 @@
+const CamelCase = require ('e01');
+
+const Panagram = require ('e02');
+
+const Marsos = require ('e03');
