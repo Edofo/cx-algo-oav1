3bd60232e5458d40c793d480e79fb216b19565e9
Nolan <Edofo@users.noreply.github.com>
2020-10-14 11:45:58 +0200

    Update e03.js

--- e03.js
+++ e03.js
@@ -1,3 +1,27 @@
-function Marsos() {
-    
-}
+function Marsos(str) {
+
+    var sos = 'SOS'.split('');
+    var marsos = [];
+
+    let count = 0;
+
+    for (var char of str) {
+
+        if (sos.includes(char) && !marsos.includes(char)) {
+            
+        } else {
+            count++;
+        }
+
+    }
+
+
+    if (marsos.sort().toString() == sos.toString()) {
+        return 'Hello Efrei'
+    } else {
+        return count;
+    }
+
+};
+
+console.log(Marsos('SOFSOSSISSOWAOSSOSSOF'));
